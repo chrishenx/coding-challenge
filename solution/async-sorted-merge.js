16fbc287b6e682e794cec4c88ec5875f203ddd4e
Christian Leon <chrishendrix24@gmail.com>
2022-04-02 09:14:57 -0600

    Async solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,33 @@
 "use strict";
+const Heap = require('heap');
+const { EntrySourcePair, compareEntrySourcePairs } = require('./log-entry-source-utils');
 
-// Print all entries, across all of the *async* sources, in chronological order.
+// Print all entries, across all of the *sync* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+module.exports = async (logSources, printer) => {
+  console.log("Starting async sort.");
+
+  const entries = new Heap(compareEntrySourcePairs);
+
+  await logSources.map(source => {
+    entries.push(new EntrySourcePair(source.last, source));
+    return source.popAsync();
   });
+
+  while (!entries.empty()) {
+    const earliestEntrySource = entries.peek();
+    const earliestEntry = earliestEntrySource.entry;
+    const earliestLogSource = earliestEntrySource.logSource;
+
+    printer.print(earliestEntry);
+
+    if (earliestLogSource.drained) {
+      entries.pop();
+    } else {
+      entries.replace(new EntrySourcePair(earliestLogSource.last, earliestLogSource));
+      await earliestLogSource.pop();
+    }
+  }
+  printer.done();
+  return console.log("Async sort complete.");
 };
