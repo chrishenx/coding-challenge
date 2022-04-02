a85429966cea93ce43fe5b683d440e4ad9ee425f
Christian Leon <chrishendrix24@gmail.com>
2022-04-02 09:14:53 -0600

    Sync solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,33 @@
 "use strict";
+const Heap = require('heap');
+const { EntrySourcePair, compareEntrySourcePairs } = require('./log-entry-source-utils');
 
-// Print all entries, across all of the sources, in chronological order.
+// Print all entries, across all of the *sync* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  console.log("Starting sync sort.");
+
+  const entries = new Heap(compareEntrySourcePairs);
+
+  logSources.forEach(source => {
+    entries.push(new EntrySourcePair(source.last, source));
+    source.pop();
+  });
+
+  while (!entries.empty()) {
+    const earliestEntrySource = entries.peek();
+    const earliestEntry = earliestEntrySource.entry;
+    const earliestLogSource = earliestEntrySource.logSource;
+
+    printer.print(earliestEntry);
+
+    if (earliestLogSource.drained) {
+      entries.pop();
+    } else {
+      entries.replace(new EntrySourcePair(earliestLogSource.last, earliestLogSource));
+      earliestLogSource.pop();
+    }
+  }
+  printer.done();
   return console.log("Sync sort complete.");
 };
